a77615135ba2ac9666e6844ee89012a998b64708
Kaustuv <kaustuv.kunal@gmail.com>
2019-10-16 23:46:26 +0530

    proc for heroku

--- app.py
+++ app.py
@@ -66,8 +66,7 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # run the app locally on the given port
-#    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 #
